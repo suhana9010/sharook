97663c3c13c2adae85b0ab2f61016e30094c1a11
Pathan Suhana <61060971+suhana9010@users.noreply.github.com>
2021-12-03 21:53:17 +0530

    Create c programs.c

---
+++ c programs.c
@@ -0,0 +1,33 @@
+Print Ascii Value of the Character
+
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    printf("ascii code= %d",c);
+    return 0;
+} 
+A-Z Ascii codes- 65-90
+a-z Ascii codes  97 -122
+
+The Greatest Number Among the Given Three Number
+
+int num1,num2,num3;
+scanf("%d %d %d",&num1,&num2,&num3);
+if(num1>num2 && num1>num3)
+    printf("greater number = %d",num1);
+else if(num2>num1 && num2>num3)
+    printf("greater number = %d",num2);
+else
+    printf("greater number = %d",num3);
+    
+The Number Is Positive or Negative
+
+int n;
+scanf("%d",&n);
+ if(n==abs(n))
+    printf("positive");
+ else
+    printf("negitive");
+  return 0;
