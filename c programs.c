ad6b8984f52abfd18d03d8efdef9c9c0f153c924
Pathan Suhana <61060971+suhana9010@users.noreply.github.com>
2021-12-08 22:00:04 +0530

    Update c programs.c

--- c programs.c
+++ c programs.c
@@ -260,3 +260,23 @@ for(int i=1;i<=n;i++){
       }
       printf("\n");
 }
+
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+int n;
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+      for(int k=1;k<=2*(n-i);k++){
+        printf(" ");
+      }
+      for(int j=1;j<=2*i-1;j++){
+          printf("*");
+          printf(" ");
+          
+           
+      }
+      printf("\n");
+}
