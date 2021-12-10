fc8701ef16ebf5ea41f55a4043c0ae45824eae3f
Pathan Suhana <61060971+suhana9010@users.noreply.github.com>
2021-12-10 22:10:53 +0530

    Update Patterns.c

--- Patterns.c
+++ Patterns.c
@@ -1,3 +1,68 @@
+Print the following pyramid
+*
+* *
+* * *
+* * * *
+* * * * *
+
+int n;
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+          printf("* ");
+      }
+    printf("\n");
+}
+
+* * * * *
+* * * *
+* * * 
+* *
+*
+int n;
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i+1;j++){
+          printf("* ");
+      }
+      printf("\n");
+}
+
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+int n;
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+      for(int k=1;k<=2*(n-i);k++){
+        printf(" ");
+      }
+      for(int j=1;j<=2*i-1;j++){
+          printf("*");
+          printf(" ");    
+      }
+      printf("\n");
+}
+
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+int n;
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+   for(int j=1;j<2*i-1;j++){
+        printf(" ");}
+   for(int k=1;k<=2*(n-i)+1;k++){
+          printf("*");
+          printf(" ");
+   }
+   printf("\n");
+}
+
 1
 1 2
 1 2 3
