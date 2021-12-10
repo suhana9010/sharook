ac721d24cd3bd2d2ffbbbb8bf68167cfc3388b66
Pathan Suhana <61060971+suhana9010@users.noreply.github.com>
2021-12-10 21:41:02 +0530

    Update Patterns.c

--- Patterns.c
+++ Patterns.c
@@ -12,3 +12,28 @@ int main(){
     }
     return 0;
 }
+
+A 
+B B 
+C C C 
+D D D D 
+E E E E E 
+F F F F F F 
+
+#include <stdio.h>
+int main() {
+   int n;
+   char ch='A';
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++){
+       for(int j=1;j<=i;j++){
+           printf("%c ",ch);
+       }
+       ch+=1;
+       printf("\n");
+       
+   }
+   
+   return 0;
+}
+
